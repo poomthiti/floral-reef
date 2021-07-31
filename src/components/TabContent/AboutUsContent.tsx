22e40a504cfaecc1e1f635d35a49c13b788cc31e
poomthiti <poom.thitipatt@gmail.com>
2021-07-31 19:30:53 +0700

    add content file

---
+++ src/components/TabContent/AboutUsContent.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { ContentContainer } from '@components';
+
+const AboutUsContent = () => {
+    return (
+        <ContentContainer>
+        </ContentContainer>
+    )
+}
